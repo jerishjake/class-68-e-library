b647bdad5653a5e2438dcfd3c8fd2a6205fc22a9
jerishjake <86720678+jerishjake@users.noreply.github.com>
2022-04-17 13:22:58 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import BottomTabNavigator from './BottomTabNavigator';
+
+
+export default function App() {
+  return (
+    <BottomTabNavigator/>
+  );
+}
+
+
---
+++ screens/Search.js
@@ -0,0 +1,30 @@
+import React,{Component} from "react"; 
+import {View,Text,StyleSheet} from 'react-native';
+
+export default class SearchScreen extends Component{
+render(){
+    return(
+        <View style={styles.container}>
+            <Text style={styles.text}>
+                Search Screen
+            </Text>
+        </View>
+    )
+}
+
+
+}
+const styles = StyleSheet.create(
+    {
+        container:{
+            flex:1,
+            justifyContent:"center",
+            alignItems:"center",
+            backgroundColor:"purple"
+        },
+        text:{
+            color:"red",
+            fontSize:25
+        }
+       }
+)
